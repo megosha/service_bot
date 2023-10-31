e73f8d4e6254a49b8b4bdf043cee6e99179a3f8c
megosha <mvgosha@gmail.com>
2023-10-31 13:55:02 +0700

    refactored sending speaker

--- send_speaker.py
+++ send_speaker.py
@@ -1,18 +1,15 @@
 from main import bot
-from service import read_speaker, valid_user_by_tid
+from service import read_speaker, valid_user_by_tid, clear_speaker
 from speakers import MEDIA
 
-def forward_theme(message):
-    bot.forward_message(MEDIA, message.chat.id, message_id=message.chat.id)
 
 tid, name = read_speaker()
 if valid_user_by_tid(tid):
     theme = bot.send_message(tid,
                      'Здравствуйте! Напишите название проповеди в ответном сообщении.'
                      '\n ⚠️Не пишите ничего, кроме темы в ответе ⚠️', )
-    # bot.register_next_step_handler(theme, forward_theme)
-    # bot.send_message(tid, 'Название отправлено')
 else:
     bot.send_message(MEDIA, f'Проповедник неизвестен')
+    clear_speaker()
 
 
