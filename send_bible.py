7c3d53b03c2d7171f1967a4e39d3fd0b01f01389
megosha <mvgosha@gmail.com>
2023-11-01 12:34:28 +0700

    small fix

--- send_bible.py
+++ send_bible.py
@@ -6,7 +6,7 @@ from speakers import MEDIA
 tid, name = read_speaker()
 if valid_user_by_tid(tid):
     theme = bot.send_message(tid,
-                     'Здравствуйте! Пришлите места Писания для проповеди на сегоняшнее служение.', )
+                     'Здравствуйте! Пришлите места Писания для проповеди на сегодняшнее служение.', )
 else:
     bot.send_message(MEDIA, f'Проповедник неизвестен. Нет мест Писания.')
     clear_speaker()
